9243c950d0e1209a0073ae0e3ee84b7b264b6908
Ben Cooper <s3560684@student.rmit.edu.au>
2018-09-30 15:37:58 +1000

    Testing select2 on form_test page

--- modules/routes/api/instrument/index.js
+++ modules/routes/api/instrument/index.js
@@ -4,10 +4,19 @@ const models = require("@modules/models");
 
 router.get("/", (req, res) => {
     Promise.resolve().then(() => {
-        var search = req.query.search || null;
+        var search = req.query.search;
         return models.list.instrument.query(search);
     }).then((instrument) => {
-        res.status(200).json(instrument).end();
+        // Convert into Select2 format and return
+        var instrumentRemap = instrument.map(row => {
+            return {
+                id: row.id,
+                text: row.name
+            };
+        });
+        res.status(200)
+            .json({results: instrumentRemap})
+            .end();
     }).catch((error) => app.handleError(error, req, res));
 });
 
