6fd90cccf0584e99c5d721b600e4d58eeaadf6de
Joshua Punshon <punshon.jm@live.com.au>
2018-10-09 17:34:37 +1000

    Fix for genres/instruments

--- www/public/js/app/user/my_profile.js
+++ www/public/js/app/user/my_profile.js
@@ -65,7 +65,13 @@ appPage.editToggle = function($this) {
 	if ( controls == "edit" ) {
 		$(".editable." + target + ".view").find("[data-id]").each(function() {
 			var id = $(this).prop("id").split("-data")[0];
-			$("#" + id).val($(this).data().id);
+
+			if ( $(this).hasClass("js-states") ) {
+				$("#" + id).val($(this).val());
+				$("#" + id).change();
+			} else {
+				$("#" + id).val($(this).data().id);
+			}
 		});
 	}
 };
@@ -78,11 +84,10 @@ appPage.editSave = function($this) {
 	$header.find("span").remove();
 
 	var data = {}, errors = [];
-	$(".editable." + target + ".edit").find("input, select").each(function() {
+	$(".editable." + target + ".edit").find("input, select").not(".js-states, .select2-search__field").each(function() {
 		var isEmpty = ( $(this).val() == "" || $(this).val() == null) ? true : false;
+		var notSame = ( $(this).val() != $("#" + $(this).prop("id") + "-data").data().id ) ? true : false;
 
-		// var notSame = ( $(this).val() != $("#" + $(this).prop("id") + "-data").data().id ) ? true : false;
-		var notSame = true; // select2 causing issue, due to varied implementation. To fix
 		if ( !$(this).hasClass("select2-search__field") ) {
 			if ( !isEmpty && notSame ) {
 				data[ $(this).prop("id") ] = $(this).val();
@@ -94,6 +99,19 @@ appPage.editSave = function($this) {
 			}
 		}
 	});
+
+	if ( $("#genres").is(":visible") ) {
+		var currentGenres = $("#genres-data").val();
+		if ( JSON.stringify(currentGenres) != JSON.stringify( $("#genres").val() ) ) {
+			data.genres = $("#genres").val();
+		}
+	}
+	if ( $("#instruments").is(":visible") ) {
+		var currentinstruments = $("#instruments-data").val();
+		if ( JSON.stringify(currentinstruments) != JSON.stringify( $("#instruments").val() ) ) {
+			data.instruments = $("#instruments").val();
+		}
+	}
 
 	if ( errors.length > 0 ) {
 		$header.append("<span class='has-danger'>Please address the errors before saving again.</span>");
@@ -115,7 +133,8 @@ appPage.editSave = function($this) {
 			if ( Object.keys(res.profile).length > 0 ) {
 				Object.keys(res.profile).map(function(key) {
 					if ( key == "genres" || key == "instruments" ) {
-
+						$("#" + id + "-data").val($("#" + key).val());
+						$("#" + id + "-data").change();
 					} else {
 						$("#" + key + "-data").text(res.profile[key]);
 						$("#" + key + "-data").data().id = res.profile[key];
