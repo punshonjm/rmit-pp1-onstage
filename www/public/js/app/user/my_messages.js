4a7de03de2d2a96bbd2c58d03ed4df6e768affb3
Ben Cooper <s3560684@student.rmit.edu.au>
2018-10-21 16:07:03 +1000

    My Messages has the basics working to be able to send.

--- www/public/js/app/user/my_messages.js
+++ www/public/js/app/user/my_messages.js
@@ -1,5 +1,7 @@
 var appPage = {};
 
+appPage.currentCaller = {};
+
 appPage.initialise = function() {
 	window.app.templates.load({ path: "messaging/thread", name: "thread" });
 };
@@ -10,11 +12,14 @@ appPage.openThread = function($this) {
 	var data = {};
 	data.thread_id = $this.data().thread;
 
+	appPage.currentThread = data.thread_id;
+
 	$.get("/api/messaging/thread", data, function(resp) {
 		console.log(resp);
 
 		$this.closest(".message").addClass("active");
 
+		appPage.currentCaller = $this;
 		let html = window.app.templates.thread(resp.thread);
 		$("#messages-panel").html(html);
 	}).fail(function(error) {
@@ -24,8 +29,31 @@ appPage.openThread = function($this) {
 	});
 };
 
+appPage.send = function($this) {
+	var data = {};
+
+	data.message_to = $this.data().id;
+	data.message = $("#new-message").val().trim();
+
+	if ( data.message.length === 0 ) {
+		// No content
+	} else {
+
+		$.post("/api/messaging/send", data, function(resp) {
+
+			appPage.openThread(appPage.currentCaller);
+
+		}, "json").fail(function(error) {
+			alert(error.responseJSON.message || "An error occured!");
+		});
+	}
+};
+
+
 $(document).ready(function() {
 	appPage.initialise();
 }).on("click", ".message", function() {
 	appPage.openThread($(this));
-})
+}).on("click", "#send-message", function() {
+	appPage.send($(this));
+});
