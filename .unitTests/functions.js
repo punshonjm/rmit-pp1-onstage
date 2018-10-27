c20a17bbef7f373e5c055e33b050691d8e7a8de0
Joshua Punshon <punshon.jm@live.com.au>
2018-10-27 12:34:26 +1000

    Fix for object values unit test

--- .unitTests/functions.js
+++ .unitTests/functions.js
@@ -5,16 +5,14 @@ const should = chai.should();
 
 describe('lTesting Object.values function', function(){
 	context('Test added core function for Objects', function() {
-		it('when passing an empty object', function() {
-			try {
-				Object.values("String")
-			} catch (err) {
-				err.should.be.an.instanceOf(TypeError)
-			}
+		it('when passing an empty object should have length of 0', function() {
+			Object.values({}).should.have.lengthOf(0);
 		});
 
 		it("When passing an object, should return array of its values", function() {
+			Object.values({ key: "value", key2: "value2" }).should.have.lengthOf(2);
 			Object.values({ key: "value", key2: "value2" }).should.be.an("Array");
+			Object.values({ key: "value", key2: "value2" })[1].should.equal("value2");
 		});
 	});
 });
