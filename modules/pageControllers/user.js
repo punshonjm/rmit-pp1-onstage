83f9a97d65d0204fd932a0d0f34236316a7eeb74
Ben Cooper <s3560684@student.rmit.edu.au>
2018-11-03 08:16:06 +1000

    User profile suspended/email verification visibility improvements.

--- modules/pageControllers/user.js
+++ modules/pageControllers/user.js
@@ -1,14 +1,28 @@
 const models = require("@modules/models");
 
-module.exports = function(req) {
+module.exports = function (req) {
 	var data = {};
 
 	return Promise.resolve().then(() => {
 		return models.users.details(req.params.id);
 	}).then((userDetails) => {
-		data.profile = userDetails;
-		data.pageName = userDetails.display_name;
-		data.user_id = req.user.user_id;
+
+		// Should we be displaying the profile?
+		data.user_visible = 1;
+		if (userDetails.account_locked === 1 || userDetails.email_verified === 0) {
+			data.user_visible = 0;
+		}
+
+		// Show if profile is visible or requesting user is an admin
+		if ((data.user_visible === 1) || (req.user.type_id === 1 && req.user.account_locked === 0)) {
+			data.profile = userDetails;
+			data.pageName = userDetails.display_name;
+			data.user_id = req.user.user_id;
+		} else {
+			data.profile = null;
+			data.pageName = "Unknown";
+			data.user_id = req.user.user_id;
+		}
 		return Promise.resolve(data);
 	});
 };
