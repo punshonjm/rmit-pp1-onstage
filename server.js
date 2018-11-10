20427e5b381d89ba2b3a820e94f47f409470b82f
Ben Cooper <s3560684@student.rmit.edu.au>
2018-11-10 20:41:52 +1000

    Updated basic CSP policy.

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ server.use(helmet.contentSecurityPolicy({
 	directives: {
  		defaultSrc: ["'self'"],
  		fontSrc: [ "'self'", 'https://fonts.googleapis.com', 'https://fonts.gstatic.com', 'https://maxcdn.bootstrapcdn.com' ],
- 		styleSrc: ["'self'", "'unsafe-inline'", 'https://maxcdn.bootstrapcdn.com', 'https://fonts.googleapis.com' ],
- 		imgSrc: ["'self'", 'https://onstage-storage.s3.ap-southeast-2.amazonaws.com'],
+ 		styleSrc: ["'self'", "'unsafe-inline'", 'https://maxcdn.bootstrapcdn.com', 'https://fonts.googleapis.com', 'https://use.fontawesome.com' ],
+ 		imgSrc: ["'self'", 'https://onstage-storage.s3.ap-southeast-2.amazonaws.com', 'https://s3-ap-southeast-2.amazonaws.com'],
 		scriptSrc: ["'self'", "'unsafe-inline'", "data:"]
  	}
 }));
